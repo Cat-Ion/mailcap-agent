bf691d4b0c8a3b0dbbb94db4df2f75c09d42cf95
Valentin Ochs <a@0au.de>
2019-05-10 23:30:57 +0200

    Use file(1) to determine the mime type

--- mailcap-client.py
+++ mailcap-client.py
@@ -1,18 +1,19 @@
-#!python3
+#!/usr/bin/python3
 import struct
 import socket
 import os
 import sys
 import mimetypes
+import subprocess
 
 s = socket.socket(socket.AF_UNIX)
 s.connect(os.path.join(os.environ["HOME"], ".mailcap.sock"))
 
-file = sys.argv[1]
-mime = mimetypes.guess_type(file)[0]
-data = open(file, "rb").read()
+filename = sys.argv[1]
+mime = subprocess.run(["file", "-i", "-b", filename], check=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
+data = open(filename, "rb").read()
 
 def strtr(x, c="B"):
     return struct.pack(c, len(x)) + x
 
-s.sendall(strtr(file.encode('utf-8')) + strtr(mime.encode('utf-8')) + strtr(data, "!I"))
+s.sendall(strtr(filename.encode('utf-8')) + strtr(mime.encode('utf-8')) + strtr(data, "!I"))
